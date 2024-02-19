32e45ac82ab2f20a229685d90d53807bbc412732
shivangi <shivangichandra806@gmail.com>
2024-02-19 17:00:47 +0530

    installed react and vite

---
+++ 01_react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Hello to the world of create-react-app </h1>
+  );
+}
+
+export default App;
---
+++ viteProject/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>hello using vite</h1>
+  )
+}
+
+export default App
